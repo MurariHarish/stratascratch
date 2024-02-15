a5be40bc3cd043082ab7a54803e6ec54819864a7
Harish Murari <114842964+MurariHarish@users.noreply.github.com>
2024-02-15 11:27:29 -0500

    Update python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -17,4 +17,30 @@ merged_df.groupby(by=['id_x','first_name'])['total_order_cost'].sum().reset_inde
 # 4. ID 10167 Total Number Of Housing Units
 housing_units_completed_us.pivot_table(index='year', values=['south', 'west', 'midwest', 'northeast'], aggfunc='sum').sum(axis=1).reset_index(name='total_num')
 
+# 5. ID 10176 Bikes Last Used
+dc_bikeshare_q1_2012.groupby(by='bike_number')['end_time'].max().reset_index().sort_values('end_time', ascending=False)
+
+# 6. ID 10310 Class Performance
+assignment_sum = box_scores.filter(regex='assignment').apply(sum,axis=1)
+assignment_sum.max() - assignment_sum.min()
+
+# 7. ID 10308 Salaries Differences
+db_merged = pd.merge(db_employee,db_dept, how = 'inner', left_on='department_id', right_on='id')
+max_salaries = db_merged.groupby('department')['salary'].max()
+abs_difference = abs(max_salaries['marketing'] - max_salaries['engineering'])
+
+# 8. ID 10301 Expensive Projects
+emp_proj_count = ms_emp_projects.groupby(by='project_id')['emp_id'].count()
+ms_merged = pd.merge(ms_projects, emp_proj_count, how='inner', left_on='id', right_on='project_id')
+ms_merged['budget_per_emp'] = round(ms_merged['budget']/ms_merged['emp_id'])
+ms_merged[['title','budget_per_emp']].sort_values('budget_per_emp', ascending=False)
+ 
+# 9. ID 10299 Finding Updated Records
+ms_employee_salary.groupby(by=['id','first_name','last_name','department_id'])['salary'].max().reset_index().sort_values('id')
+
+
+
+
+
+
 
